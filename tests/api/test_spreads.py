8760604598fcf7af1faef0f7d3b2a2b9a71445e2
andres <and.sanchezc@outlook.com>
2024-02-12 00:29:19 -0300

    fix test spreads

--- tests/api/test_spreads.py
+++ tests/api/test_spreads.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import AsyncMock, patch
-from api.spreads import get_spread, get_spreads
+from api.spreads import get_spread, get_spreads, SpreadResponseModel
 
 class TestSpreads(unittest.IsolatedAsyncioTestCase):
 
@@ -9,7 +9,7 @@ class TestSpreads(unittest.IsolatedAsyncioTestCase):
         mock_calculate_spread = AsyncMock(return_value=0.05)
         with patch('api.spreads.calculate_spread', mock_calculate_spread):
             result = await get_spread(market)
-            self.assertEqual(result, {"market": market, "spread": 0.05})
+            self.assertEqual(result, SpreadResponseModel(market= market, spread= 0.05))
             mock_calculate_spread.assert_called_once_with(market)
 
     async def test_get_spreads(self):
@@ -18,7 +18,7 @@ class TestSpreads(unittest.IsolatedAsyncioTestCase):
         with patch('api.spreads.get_all_markets', mock_get_all_markets), \
              patch('api.spreads.calculate_spread', mock_calculate_spread):
             result = await get_spreads()
-            expected_result = [{"market": "BTC-CLP", "spread": 0.05}, {"market": "ETH-BTC", "spread": 0.05}]
+            expected_result = [SpreadResponseModel(market= "BTC-CLP", spread= 0.05), SpreadResponseModel(market= "ETH-BTC", spread= 0.05)]
             self.assertEqual(result, expected_result)
             mock_get_all_markets.assert_called_once()
             mock_calculate_spread.assert_any_call("BTC-CLP")
